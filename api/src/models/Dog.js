6d5dc14f50911d9460c6cb6f8f05c6df8cf7f73b
ricardo <rblazquez111@gmail.com>
2023-09-05 23:04:37 -0300

    creo la tabla dog y temperament

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,12 +1,35 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "dog",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      image: { type: DataTypes.STRING },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      weight: { type: DataTypes.STRING },
+      height: { type: DataTypes.STRING },
+      life_span: { type: DataTypes.STRING },
+      cretated: { type: DataTypes.BOOLEAN, defaultValue: true },
     },
-  });
+    { timestamps: false }
+  );
 };
+
+/* iD.*
+Imagen.*
+Nombre.*
+Peso.*
+Altura.*
+Años de vida.* */
